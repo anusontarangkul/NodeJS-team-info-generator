f3464931d599e7346e66f217540aced72c6d3b6b
David Anusontarangkul <anusontarangkul.d@gmail.com>
2020-10-13 20:21:21 -0700

    add built team functions

--- Develop/app.js
+++ Develop/app.js
@@ -35,31 +35,6 @@ const teamMembers = [];
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
 
-// const questions = [
-//     {
-//         type: "input",
-//         name: "name",
-//         message: "What is your name?"
-//     },
-//     {
-//         type: "input",
-//         name: "Id",
-//         message: "What is your Id?"
-//     },
-//     {
-//         type: "input",
-//         name: "email",
-//         message: "What is your email?"
-//     },
-//     {
-//         type: "list",
-//         name: "role",
-//         message: "What is your role?"
-//         choices: ["Manager", "Engineer", "Intern"]
-//     }
-
-
-// ]
 
 function appMenu() {
     function createManager() {
@@ -67,22 +42,50 @@ function appMenu() {
             {
                 type: "input",
                 name: "managerName",
-                message: "What is your name?"
+                message: "What is your name?",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true
+                    }
+                    return "Please enter at least one character."
+                }
             },
             {
                 type: "input",
                 name: "managerID",
-                message: "What is your Id?"
+                message: "What is your Id?",
+                validate: answer => {
+                    const pass = answer.match(/^[1-9]\d*$/);
+                    if (pass) {
+                        return true;
+                    }
+                    return "Please enter a postive number greater than zero."
+                }
+
             },
             {
                 type: "input",
                 name: "managerEmail",
-                message: "What is your email?"
+                message: "What is your email?",
+                validate: answer => {
+                    const pass = answer.match(/[a-z0-9\._%+!$&*=^|~#%{}/\-]+@([a-z0-9\-]+\.){1,}([a-z]{2,22})/);
+                    if (pass) {
+                        return true;
+                    }
+                    return "Please enter a valid email adress."
+                }
             },
             {
                 type: "input",
                 name: "managerOfficeNumber",
-                message: "What is your office number?"
+                message: "What is your office number?",
+                validate: answer => {
+                    const pass = answer.match(/^[1-9]\d*$/);
+                    if (pass) {
+                        return true;
+                    }
+                    return "Please enter a postive number greater than zero."
+                }
             }
         ]).then(function (answers) {
             const manager = new Manager(answers.managerName, answers.managerID, answers.managerEmail, answers.managerOfficeNumber)
@@ -113,14 +116,76 @@ function appMenu() {
 
     }
     function createEngineer() {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "engineerName",
+                message: "What is your name?",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true
+                    }
+                    return "Please enter at least one character."
+                }
+            },
+            {
+                type: "input",
+                name: "engineerID",
+                message: "What is your Id?"
+            },
+            {
+                type: "input",
+                name: "engineerEmail",
+                message: "What is your email?"
+            },
+            {
+                type: "input",
+                name: "engineerGithub",
+                message: "What is your Github?"
+            }
 
+        ]).then(function (answers) {
+            const engineer = new Engineer(answers.engineerName, answers.engineerID, answers.engineerEmail, answers.engineerGithub)
+            teamMembers.push(engineer);
+            createTeam();
+        })
     }
 
     function createIntern() {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "internName",
+                message: "What is your name?"
+            },
+            {
+                type: "input",
+                name: "internID",
+                message: "What is your Id?"
+            },
+            {
+                type: "input",
+                name: "internEmail",
+                message: "What is your email?"
+            },
+            {
+                type: "input",
+                name: "internSchool",
+                message: "What is your School?"
+            }
+
+        ]).then(function (answers) {
+            const intern = new Intern(answers.internName, answers.internID, answers.internEmail, answers.internSchool)
+            teamMembers.push(intern);
+            createTeam();
+        })
 
     }
     function builtTeam() {
-
+        if (!fs.existsSync(OUTPUT_DIR)) {
+            fs.mkdirSync(OUTPUT_DIR)
+        }
+        fs.writeFileSync(outputPath, render(teamMembers), "utf-8")
     }
     createManager();
 
